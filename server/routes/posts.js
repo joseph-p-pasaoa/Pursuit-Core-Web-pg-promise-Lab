e3f492c358a02ead8f36d894ce4e2e2b17bcbd63
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-03 17:49:36 -0500

    0.3, all posts routes functional, middleware revision start

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -23,42 +23,45 @@ const express = require('express');
 const db = require('../db.js');
 
 
-/* ROUTES */ // GO ES5 GO!
-router.get('/all', (req, res) => {
-    db.any("SELECT * FROM posts")
-      .then(data => {
-          res.json({
-              status: "ff6 SUCCESS music plays",
-              payload: data
-          });
-      })
-      .catch(err => {
+/* MIDDLEWARE */
+const getUsers = (req, res, next) => {
+  let insertQuery = `SELECT * FROM posts`;
+  if (req.params.user_id) {
+    insertQuery += ` WHERE poster_id = $1`;
+  }
+  db.any(insertQuery, req.params.user_id)
+    .then(data => {
         res.json({
-            status: "ff6 FAIL gameover music plays",
-            message: err
+            status: "ff6 SUCCESS music plays",
+            payload: data
         });
+    })
+    .catch(err => {
+      res.json({
+          status: "ff6 FAIL gameover music plays",
+          message: err
       });
-});
+  });
+}
 
-router.get('/:user_id', (req, res) => {
-    db.any("SELECT * FROM posts WHERE poster_id = $1", req.params.user_id)
-      .then(data => {
-          res.json({
-              status: "ff6 SUCCESS music plays",
-              payload: data
-          });
-      })
-      .catch(err => {
-          res.json({
-              status: "ff6 FAIL gameover music plays",
-              message: err
-          });
-      });
-});
+const addUser = (req, res, next) => {
+  const insertQuery = `
+    INSERT INTO users (firstName, lastName, age) VALUES
+      ($1, $2, $3);
+    `;
+  db.none(insertQuery, [req.body.firstName, req.body.lastName, req.body.age]);
+  res.json({
+      status: "ff6 SUCCESS music plays",
+      message: "user created",
+      submitted: req.body
+  });
+}
 
-router.post('/register', (req, res) => {
-    res.send('Creating new post!');
-});
+
+/* ROUTES */ // GO ES5 GO!
+router.get('/all', getUsers);
+router.get('/:user_id', getUsers);
+router.post('/register', addUser); // SHOULD NOT PREVENT MULTIPLE IDENTICAL NAMES
 
 
 module.exports = router;
